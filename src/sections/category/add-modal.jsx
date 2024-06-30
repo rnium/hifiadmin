60e9c91f16113fe516e9d5c56ec4c25f465c2cd8
HifiComputer <rniumo@gmail.com>
2024-06-30 18:10:01 +0600

    add modal complete

--- src/sections/category/add-modal.jsx
+++ src/sections/category/add-modal.jsx
@@ -1,9 +1,43 @@
 import { Modal } from "antd";
+import { useState } from "react";
 import propTypes from 'prop-types'
 
-import { Stack, Button, TextField, Typography } from "@mui/material";
+import { Stack, Button, Select, MenuItem, TextField, InputLabel, Typography, FormControl } from "@mui/material";
 
-function AddCatModal({ open, setOpen }) {
+const cat_types = [
+    {
+        title: 'General Category',
+        code: 'general'
+    },
+    {
+        title: 'Brand Category',
+        code: 'brand'
+    },
+    {
+        title: 'Feature Category',
+        code: 'feature'
+    },
+    {
+        title: 'Tag Under Feature',
+        code: 'tag'
+    },
+]
+
+function AddCatModal({ open, setOpen, parentType }) {
+    const [formData, setFormData] = useState({
+        title: '',
+        type: 'general'
+    });
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = () => {
+        console.log(formData);
+    }
+    // const 
     return (
         <Modal
             open={open}
@@ -13,15 +47,43 @@ function AddCatModal({ open, setOpen }) {
             <Typography>Add Subcategory</Typography>
             <Stack
                 alignItems="flex-end"
-                spacing={1}
-                sx={{ mt: 1 }}
+                spacing={1.4}
+                sx={{ mt: 2 }}
             >
                 <TextField
                     label="Sub Category Name"
-                    variant='filled'
+                    name="title"
+                    onChange={handleChange}
                     fullWidth
                 />
-                <Button variant='contained'>Add</Button>
+                <FormControl fullWidth >
+                    <InputLabel id="type-label">Category Type</InputLabel>
+                    <Select
+                        labelId="type-label"
+                        id="type"
+                        name="type"
+                        value={formData.type}
+                        label="Category Type"
+                        onChange={handleChange}
+                    >
+                        {
+                            cat_types.map((ct, idx) => (
+                                <MenuItem
+                                    value={ct.code}
+                                >
+                                    {ct.title}
+                                </MenuItem>
+                            ))
+                        }
+                    </Select>
+                </FormControl>
+                <Button
+                    disabled={formData.title.length === 0}
+                    variant='contained'
+                    onClick={handleSubmit}
+                >
+                    Add
+                </Button>
             </Stack>
         </Modal>
     )
@@ -32,4 +94,5 @@ export default AddCatModal;
 AddCatModal.propTypes = {
     open: propTypes.bool,
     setOpen: propTypes.any,
+    parentType: propTypes.any,
 }
